e7cc328bec2e5c5ac8b935f77c54873efa5ecc67
Rinnabel <55181896+Rinnabel@users.noreply.github.com>
2020-07-30 09:34:24 +0700

    up

--- tentukan-deret-geometri.php
+++ tentukan-deret-geometri.php
@@ -1,11 +1,25 @@
 <?php
 function tentukan_deret_geometri($arr) {
 // kode di sini
+    $ratio = $arr[1]/$arr[0];
+   
+    for($i=1; $i<sizeof($arr); $i++)
+    {
+        if (($arr[$i]/($arr[$i-1])) != $ratio)
+        {
+            return "False";
+        }
+    }        
+  return "True";
 }
 //TEST CASES
 echo tentukan_deret_geometri([1, 3, 9, 27, 81]); // true
+echo "<br>";
 echo tentukan_deret_geometri([2, 4, 8, 16, 32]); // true
+echo "<br>";
 echo tentukan_deret_geometri([2, 4, 6, 8]); // false
+echo "<br>";
 echo tentukan_deret_geometri([2, 6, 18, 54]); // true
+echo "<br>";
 echo tentukan_deret_geometri([1, 2, 3, 4, 7, 9]); //false
 ?>
